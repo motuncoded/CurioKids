e629b0dfa74ca0c0804d0818db32eedf5adcad31
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-04-20 20:44:34 +0100

    resolve: deployment issues

--- vite.config.js
+++ vite.config.js
@@ -8,31 +8,5 @@ export default defineConfig({
   base: "/CurioKids/",
   plugins: [
     react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "CurioKids",
-        short_name: "CurioKids",
-        description:
-          "A fun way for kids to learn colors, numbers, and objects!",
-        theme_color: "#6C63FF",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
   ],
 });
